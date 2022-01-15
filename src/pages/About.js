2cfe89274dd12cf8de7fca1cecd725e4142735c3
MarmotaBlanoasa <cristirares@yahoo.com>
2022-01-15 15:41:13 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis interdum magna. Nulla egestas eu nunc a semper. Quisque suscipit suscipit malesuada. Sed vel augue finibus, sollicitudin enim eu, sodales.
+      </p>
+    </section>
+  )
+}
+
+export default About
